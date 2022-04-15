046af0f4727b28aa6b2d51f284730f1a4b71ae41
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-15 22:42:54 +0200

    Links Open In New Tabs

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -7,7 +7,7 @@ const Card = (props) => {
 				<img className='Card_Image' src={props.Img} alt=' '></img>
 				<h2 className='Card_Type'>{props.Type}</h2>
 				<p className='Card_Description'>{props.Description}</p>
-				<a href={props.Download} className='Btn'>
+				<a href={props.Download} className='Btn' target="_blank">
 					<span>
 						Download
 					</span>
