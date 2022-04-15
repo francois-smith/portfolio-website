140c446466200141142e88333e3646aee084cfb7
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-15 17:31:24 +0200

    Added ABout Cards Styling

--- src/Components/About/AboutStyles.js
+++ src/Components/About/AboutStyles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+
 export const AboutSection = styled.div`
     background-color:  ${props => props.theme.colors.background1};
     height: auto;
@@ -7,7 +8,43 @@ export const AboutSection = styled.div`
 
 export const AboutParagraph = styled.p`
     padding-top: 20px;
+    font-size: 20px;
     width: 55%;
     margin: auto;
     text-align: center;
+`;
+
+export const AboutCards = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-bottom: 20px;
+`;
+
+export const AboutCard = styled.div`
+    flex-basis: 35%;
+    margin: 25px;
+`;
+
+export const AboutCardTitle = styled.div`
+    display: flex;
+    align-items: center;
+
+    h3{
+        letter-spacing: 0.15em;
+        font-size: 28px;
+        font-weight: 600;
+        transform: translateX(-20px);
+    }
+    svg{
+        background-blend-mode: luminosity;
+    }
+`;
+
+
+export const AboutCardDescription = styled.p`
+    font-weight: 300;
+    font-size: 18px;
+    padding-left: 40px;
+    padding-top: 15px;
 `;
