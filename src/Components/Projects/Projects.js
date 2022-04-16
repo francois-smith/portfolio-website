617cdd3739236247157955540e5c9b6a6e53bac2
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-16 14:33:44 +0200

    Added New Project

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '../Card/Card';
 import ProjectThumbnail from '../../Media/Images/ProjectThumbnail.png'
 import INLThumbnail from '../../Media/Images/INLThumbnail.png'
+import NewsLodgeThumbnail from '../../Media/Images/NewsLodge.png'
 import { ProjectSection } from './ProjectsStyles';
 import { CardsContainer } from '../Card/CardStyles';
 
@@ -11,8 +12,9 @@ const Projects = () => {
         <ProjectSection id='ProjectsSection'> 
             <h2 class='Section_Heading'>PROJECTS</h2>
             <CardsContainer>
-                <Card Type='DESIGNED & CODED' Img={ProjectThumbnail} Description='You can download the code and files for this website using the button below' Download='https://github.com/francois-smith/portfolio-website'/>
-                <Card Type='CODED' Img={INLThumbnail} Description='Website designed by a group of multimedia students for our module INL 120' Download='https://github.com/Multimedia-Overachievers/INL120'/>
+                <Card Type='DESIGNED & CODED' Img={ProjectThumbnail} Description='You can download the code and files for this website using the button below' DownloadText="Download" Download='https://github.com/francois-smith/portfolio-website'/>
+                <Card Type='DESIGNED & CODED' Img={NewsLodgeThumbnail} Description='News site made for COS216. Full stack website using the LAMP stack' DownloadText="Coming Soon" Download=''/>
+                <Card Type='CODED' Img={INLThumbnail} Description='Website designed by a group of multimedia students for our module INL 120' DownloadText="Download" Download='https://github.com/Multimedia-Overachievers/INL120'/>
             </CardsContainer>
             <p id="Project_Footer">And many more to come!</p>
         </ProjectSection>
