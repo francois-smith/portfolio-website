77844e375931cfcef45ed0c019d5d248f55b077c
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-18 01:09:20 +0200

    Added New Projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -12,9 +12,9 @@ const Projects = () => {
         <ProjectSection id='ProjectsSection'> 
             <h2 class='Section_Heading'>PROJECTS</h2>
             <CardsContainer>
-                <Card Type='DESIGNED & CODED' Img={ProjectThumbnail} Description='You can download the code and files for this website using the button below' DownloadText="Download" Download='https://github.com/francois-smith/portfolio-website'/>
-                <Card Type='DESIGNED & CODED' Img={NewsLodgeThumbnail} Description='News site made for COS216. Full stack website using the LAMP stack' DownloadText="Coming Soon" Download=''/>
-                <Card Type='CODED' Img={INLThumbnail} Description='Website designed by a group of multimedia students for our module INL 120' DownloadText="Download" Download='https://github.com/Multimedia-Overachievers/INL120'/>
+                <Card Type='DESIGNED & CODED' Img={ProjectThumbnail} Description='You can download the code and files for this website using the button below' DownloadText="Download" Download='https://github.com/francois-smith/portfolio-website' shouldHide/>
+                <Card Type='DESIGNED & CODED' Img={NewsLodgeThumbnail} Description='News site made for COS216. Full stack website using the LAMP stack' DownloadText="Coming Soon" Download='' linkTo="" shouldHide/>
+                <Card Type='CODED' Img={INLThumbnail} Description='Website designed by a group of multimedia students for our module INL 120' DownloadText="Download" Download='https://github.com/Multimedia-Overachievers/INL120' linkTo="https://inl-120.netlify.app/"/>
             </CardsContainer>
             <p id="Project_Footer">And many more to come!</p>
         </ProjectSection>
