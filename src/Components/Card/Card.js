60e0dd3473d2491b9d173b7526d7353fa2585647
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-18 01:08:30 +0200

    Added Additional optional button

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -7,11 +7,18 @@ const Card = (props) => {
 				<img className='Card_Image' src={props.Img} alt=' '></img>
 				<h2 className='Card_Type'>{props.Type}</h2>
 				<p className='Card_Description'>{props.Description}</p>
-				<a href={props.Download} className='Btn' target="_blank">
-					<span>
-						{props.DownloadText}
-					</span>
-				</a>
+				<div className='Buttons'>
+					<a  href={props.linkTo} className={`Btn ${props.shouldHide? 'hidden' : undefined}`} target="_blank">
+						<span>
+							View Now
+						</span>
+					</a>
+					<a href={props.Download} className='Btn' target="_blank">
+						<span>
+							{props.DownloadText}
+						</span>
+					</a>
+				</div>
 			</div>
 		</div>
     );
