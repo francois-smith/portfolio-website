0b92f71e32176d95522daf9d26ae5e4d09a05a33
Francois Smith <steamfaffa@gmail.com>
2022-07-20 00:34:05 +0200

    New THeme Sizes

--- src/Styles/theme.tsx
+++ src/Styles/theme.tsx
@@ -35,12 +35,12 @@ const DefaultTheme: ThemeClass = {
         text: "#f0f0f0"
     },
     breakpoints: {
-        xs: 'screen and (max-width: 395px)',
-        sm: 'screen and (max-width: 520px)',
-        md: 'screen and (max-width: 768px)',
+        xs: 'screen and (max-width: 320px)',
+        sm: 'screen and (max-width: 568px)',
+        md: 'screen and (max-width: 800px)',
         lg: 'screen and (max-width: 1024px)',
-        xl: 'screen and (max-width: 1280px)',
-        xxl: 'screen and (max-width: 1600px)'
+        xl: 'screen and (max-width: 1300px)',
+        xxl: 'screen and (max-width: 1440px)',
     }
 }
 
