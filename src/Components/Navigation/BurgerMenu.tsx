5e04544314610e43b14b317879c4fb09c20359dc
Francois Smith <steamfaffa@gmail.com>
2022-07-20 00:33:23 +0200

    Added Burger Menu Class File

---
+++ src/Components/Navigation/BurgerMenu.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { slide as Menu } from 'react-burger-menu'
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+class Burger extends React.Component<{}, {menuOpen: false}> {
+	constructor(props: any) {
+		super(props)
+		this.state = {
+		  	menuOpen: false
+		}
+	}
+
+	handleStateChange(state: any) {
+		this.setState({menuOpen: state.isOpen}) ;
+	}
+
+	closeMenu() {
+		this.setState({menuOpen: false});
+	}
+
+  	render () {
+		return (
+	  		<Menu isOpen={this.state.menuOpen} onStateChange={(state) => this.handleStateChange(state)} right customBurgerIcon={<img src="/Media/SVG/burger-icon.svg" alt="Burger Icon" /> }>
+				<a onClick={() => this.closeMenu()} className="menu-item" href="#ProjectsSection">Project</a>
+				<a onClick={() => this.closeMenu()} className="menu-item" href="#TechnologiesSection">Technologies</a>
+				<a onClick={() => this.closeMenu()} className="menu-item" href="#AboutSection">About</a>
+				<a onClick={() => this.closeMenu()} className="menu-item" href="#ContactSection">Contact</a>
+                <div>
+                    <a href='https://github.com/francois-smith'>
+                        <AiFillGithub className='svg_icon'></AiFillGithub>
+                    </a>
+                    <a href='https://instagram.com/_francois.smith_'>
+                        <AiFillInstagram className='svg_icon'></AiFillInstagram>              
+                    </a>
+                    <a href='https://www.linkedin.com/in/francois-smith-b02609210/'>
+                        <AiFillLinkedin className='svg_icon'></AiFillLinkedin>
+                    </a>
+                </div>
+                <div className="bm-cv-button">
+                    <div>
+                        <span className="Text-Gradient">View CV</span>
+                    </div>
+                </div>
+	  		</Menu>
+		);
+  	}
+}
+
+export default Burger;
