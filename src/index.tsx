a697a8ccec2cd5f6301aed7cbe702d1bf26f99c0
Francois Smith <steamfaffa@gmail.com>
2022-07-20 16:49:48 +0200

    Hide Burger When Scrolling Down

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+let lastScroll = window.scrollY;
+window.onscroll = function() {
+	const burger = document.getElementsByClassName(
+		'bm-burger-button',
+	) as HTMLCollectionOf<HTMLElement>;
+	if(window.scrollY > lastScroll) {
+		burger[0].style.opacity = "0";
+	}
+	else {
+		burger[0].style.opacity = "1";
+	}
+	lastScroll = window.scrollY;
+} 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
