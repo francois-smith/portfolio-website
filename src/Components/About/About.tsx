5d539deb75165e47e8583fa4c82e3ccb75d2334d
Francois Smith <steamfaffa@gmail.com>
2022-07-20 13:20:44 +0200

    Updated About

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -6,38 +6,19 @@ const Card: React.FC = () => {
        <AboutSection id='AboutSection'>
             <h2 className='Section_Heading'>ABOUT ME</h2>
             <AboutParagraph>  
-                I am a developer based in South Africa. I have had a passion for all forms of coding since my very first 
-                “Hello World”. My dream is to build innovative products that bring joy to the people using them.
+                Hey there! I am a developer based in South Africa working on all forms of digital products, while loving what i do!
             </AboutParagraph>
-            <svg className="GradientUnderline" width="531" height="16" viewBox="0 0 531 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g filter="url(#filter0_d_374_87)">
-                     <path d="M8 0C5.79086 1.93129e-07 4 1.79086 4 4C4 6.20914 5.79086 8 8 8L8 0ZM523 7.99995C525.209 7.99995 527 6.20909 527 3.99995C527 1.79082 525.209 -4.52159e-05 523 -4.50227e-05L523 7.99995ZM8 8L523 7.99995L523 -4.50227e-05L8 0L8 8Z" fill="url(#paint0_linear_374_87)"/>
-                </g>
-                <defs>
-                    <filter id="filter0_d_374_87" x="0" y="0" width="531" height="16" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                        <feOffset dy="4"/>
-                        <feGaussianBlur stdDeviation="2"/>
-                        <feComposite in2="hardAlpha" operator="out"/>
-                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_374_87"/>
-                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_374_87" result="shape"/>
-                    </filter>
-                        <linearGradient id="paint0_linear_374_87" x1="-2.61855" y1="4" x2="0.301908" y2="44.3294" gradientUnits="userSpaceOnUse">
-                            <stop stop-color="#00ADEE"/>
-                            <stop offset="0.578125" stop-color="#584895"/>
-                    </linearGradient>
-                </defs>
-            </svg>
+            <div className="about-divider">
+                <img src="/Media/SVG/divider.svg" alt=""/>
+            </div>
             <AboutCards>
                 <AboutCard>
                     <AboutCardTitle>
-                        <img src="/Media/Images/Design.png'"></img>
+                        <img src="/Media/Images/Design.png"></img>
                         <h3>DESIGN</h3>
                     </AboutCardTitle>
                     <AboutCardDescription>
-                        I can design the site from scratch within figma according to your desires and needs. I can also design custom logos and vectors.
+                        Not a designer? No problem! I'll design your website in Figma according to your needs and tweak the parts you don't like. I will help you make an aesthetically beautiful website. I am also a logo designer.
                     </AboutCardDescription>
                 </AboutCard>
                 <AboutCard>
@@ -46,7 +27,7 @@ const Card: React.FC = () => {
                         <h3>DEVELOPMENT</h3>
                     </AboutCardTitle>
                     <AboutCardDescription>
-                        I can develop the website using HTML, CSS, JavaScrip and Angular/React. The code will be hand written by me, no templates.
+                        Leave the development to me. Using tools and libraries such as React and Styled Components I'll build you a functional and performant website. All code is handwritten by me, with no generic templates.
                     </AboutCardDescription>
                 </AboutCard>
                 <AboutCard>
@@ -55,7 +36,7 @@ const Card: React.FC = () => {
                         <h3>MAINTENANCE</h3>
                     </AboutCardTitle>
                     <AboutCardDescription>
-                        I will setup the initial hosting and uploading of your website, as well as the required updates based on the project.
+                        Unsure what to do once you have a functional website? I'll help you set up hosting and your domain. Keep it up and running with occasional updates to the codebase if needed.
                     </AboutCardDescription>
                 </AboutCard>
             </AboutCards>
