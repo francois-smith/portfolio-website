f4f6948731a22b049c46328d83a1757db286022c
Francois Smith <steamfaffa@gmail.com>
2022-07-20 16:50:36 +0200

    Reworked Project Cards

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,10 +1,87 @@
 import React from 'react';
-import { ProjectSection } from './ProjectsStyles';
+import { ProjectSection, ProjectsContainer, Project } from './ProjectsStyles';
+import { FiGithub, FiExternalLink } from "react-icons/fi";
+
+const projects = [
+    {
+        type: "Designed & Coded",
+        title: "Portfolio Website",
+        description: "The website you are currently viewing! Built to showcase my work and to get in contact with me. ",
+        tags: [
+            "Styled Components", "React", "Typescript", "React Icons"
+        ],
+        github_link: "https://github.com/francois-smith/portfolio-website",
+        link: "https://francois-smith.com",
+        image: {
+            path: "/Media/Projects/portfolio.png",
+            alt: "",
+        }
+    },
+    {
+        type: "Designed & Coded",
+        title: "M Photography",
+        description: "Portfolio and showcase website for a local photographer. Main features include a responsive mosaic photo gallery used to showcase her work.",
+        tags: [
+            "Styled Components", "React", "React Router", "React Icons"
+        ],
+        github_link: "https://github.com/francois-smith/M-Photography",
+        "link": "https://maryna-m-photography.com",
+        image: {
+            path: "/Media/Projects/mphotography.png",
+            alt: "",
+        }
+    },
+    {
+        type: "Designed",
+        title: "Moonrealm Productions",
+        description: "Group project I did in my first year of university. The module required us to create a website for a game studio, this was my group's submission.",
+        tags: [
+            "CSS3", "Javascript", "University Project", "HTML"
+        ],
+        github_link: "https://github.com/Multimedia-Overachievers/INL120",
+        "link": "https://inl-120.netlify.app/",
+        image: {
+            path: "/Media/Projects/moonrealm.png",
+            alt: "",
+        }
+    },
+]
 
 const Projects: React.FC = () => {
     return (
         <ProjectSection id='ProjectsSection'> 
             <h2 className='Section_Heading'>PROJECTS</h2>
+            <ProjectsContainer>
+                {projects.map((project, key) =>{
+                    return (
+                        <Project key={key}>
+                            <div className='project-content'>
+                                <div className='project-header'>
+                                    <h3 className='project-type'><span className='Text-Gradient'>{project.type}</span></h3>
+                                    <a href={project.link}><h2 className='project-title'>{project.title}</h2></a>
+                                </div>
+                                <div className='project-description'>
+                                    <p>{project.description}</p>
+                                </div>
+                                <div className='project-tags'>
+                                    {project.tags.map(tag =>{
+                                        return (
+                                            <span className='project-tag'>{tag}</span>
+                                        );
+                                    })}
+                                </div>
+                                <div className='project-links'>
+                                    <a href={project.github_link}><FiGithub size={22} className='project-link'/></a>
+                                    <a href={project.link}><FiExternalLink size={22} className='project-link'/></a>
+                                </div>
+                            </div>
+                            <div className="project-image">
+                            <a href={project.link}><img src={project.image.path} alt={project.image.alt}/></a>
+                            </div>
+                        </Project>
+                    );
+                })}
+            </ProjectsContainer>
             <p id="Project_Footer">And many more to come!</p>
         </ProjectSection>
     );
--- src/Components/Projects/ProjectsStyles.tsx
+++ src/Components/Projects/ProjectsStyles.tsx
@@ -11,8 +11,212 @@ export const ProjectSection = styled.div`
         padding-top: 20px;
     }
 
+    @media ${props => props.theme.breakpoints.sm}{
+        #Project_Footer {
+            font-size: 20px;
+        }
+    }
+
     @media ${props => props.theme.breakpoints.xs}{
-        font-size: 16px;
+        #Project_Footer {
+            font-size: 17px;
+        }
     }
 `;
+
+export const ProjectsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    div:nth-of-type(2n) {
+        align-items: flex-end;
+        text-align: right;
+
+        .project-links{
+            justify-content: flex-end;
+        }
+        .project-tags{
+            justify-content: flex-end;
+        }
+        .project-image{
+            left: 0px;
+        }
+        .project-description{
+            text-align: right;
+        }
+    }
+
+    @media ${props => props.theme.breakpoints.md}{
+        div:nth-of-type(2n) {
+            align-items: flex-start;
+            text-align: left;
+
+            .project-links{
+                justify-content: flex-start;
+            }
+            .project-tags{
+                justify-content: flex-start;
+            }
+            .project-image{
+                right: 0px;
+            }
+            .project-description{
+                text-align: left;
+            }
+        }
+    }
+`;
+
+export const Project = styled.div`
+    width: 90%;
+    max-width: 1000px;
+    margin-bottom: 100px;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    position: relative;
+    overflow: hidden;
+
+    &:hover{
+        .project-header{
+            transform: translateY(-10px);
+        }
+
+        .project-image{
+            filter: grayscale(100%) contrast(1) brightness(88%);
+        }
+        
+    }
+
+    .project-content{
+        width: 55%;
+        position: relative;
+        z-index: 2;
+
+        a{
+            text-decoration: none;
+        }
+        .project-header{
+            transition: all 0.25s;
+        }
+        .project-type{
+            font-weight: 400;
+            font-size: 16px;
+            pointer-events: none;
+        }
+        .project-title{
+            font-weight: 600;
+            font-size: clamp(24px, 5vw, 28px);
+            margin-top: 5px;    
+            color:  ${props => props.theme.colors.text};
+            transition: all 0.25s;        
+        }
+        .project-description{
+            pointer-events: none;
+            padding: 30px;
+            background-color:  ${props => props.theme.colors.background2};
+            font-weight: 400;
+            font-size: 18px;
+            border-radius: 5px;
+            margin: 25px 0px 15px;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+            text-align: left;
+        }
+        .project-tags{
+            margin-bottom: 10px;
+            text-align: left;
+            display: flex;
+            flex-wrap: wrap;
+            pointer-events: none;
+        }
+        .project-tag{
+            padding: 4px 8px;
+            font-size: 14px;
+        }
+        .project-links{
+            display: flex;
+
+            .project-link{
+                color:  ${props => props.theme.colors.text};
+                margin: 5px;
+
+                &:hover{
+                    color: ${props => props.theme.colors.primary};
+                }
+            }
+        }
+    }
+    .project-image{
+        width: 50%;
+        position: absolute;
+        z-index: 1;
+        top: 50%;
+        transform: translateY(-50%);
+        right: 0px;
+
+        img{
+            height: 100%;
+            width: 100%;
+
+            mix-blend-mode: multiply;
+            filter: grayscale(100%) contrast(1) brightness(80%);
+        }
+    }
+
+    @media ${props => props.theme.breakpoints.md}{
+        margin-bottom: 60px;
+
+        border-radius: 4px;
+        background-color:  ${props => props.theme.colors.background2};
+
+        .project-content{
+            padding: 25px;
+            width: 100%;
+
+            .project-type{
+                font-size: 15px;
+            }
+            .project-title{
+                font-size: clamp(22px, 5vw, 26px);
+            }
+            .project-description{
+                padding: 0px;
+                background-color: rgba(255, 255, 240, 0);
+                font-size: 16px;
+                box-shadow: none;
+            }
+            .project-tag{
+                padding: 0px 16px 0px 0px;
+                font-size: 13px;
+            }
+        }
+        .project-image{
+            display: none;
+        }
+    }
+
+    @media ${props => props.theme.breakpoints.sm}{
+        margin-bottom: 40px;
+
+        .project-content{
+            padding: 15px;
+            width: 100%;
+
+            .project-type{
+                font-size: 14px;
+            }
+            .project-title{
+                font-size: clamp(20px, 5vw, 25px);
+            }
+            .project-description{
+                font-size: 15px;
+            }
+            .project-tag{
+                padding: 2px 16px 2px 0px;
+            }
+        }
+    }
+`;
+
 
